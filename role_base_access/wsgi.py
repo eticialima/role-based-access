c8fba11b23bc1ffde70df7fb62e0363daf7d5951
eticialima <leticialimacgi@gmail.com>
2026-04-27 17:58:37 -0300

    cria validação de acessos e grupos / permissoes

--- role_base_access/wsgi.py
+++ role_base_access/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RoleBasedAccess project.
+WSGI config for role_base_access project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RoleBasedAccess.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'role_base_access.settings')
 
 application = get_wsgi_application()
